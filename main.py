0ba02d2f6dede6df1b2482b42ae580b4662a6f4a
David <davidnikishov1324@gmail.com>
2025-03-11 13:06:16 +0200

    По нужному классу находим значения. Преобразуем цену в число

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = "https://finance.ua/ua/crypto"
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
+
+coins = []
+for code_tag, name_tag, price_tag in zip(
+        soup.find_all("p", class_="CoinsListstyles__Code-sc-1c8245s-24 lgioIM"),
+        soup.find_all("p", class_="CoinsListstyles__Name-sc-1c8245s-23 kVPjIs"),
+        soup.find_all("td", class_="CoinsListstyles__Price-sc-1c8245s-25 fYNuHQ")):
+    
+    code = code_tag.get_text(strip=True)
+    name = name_tag.get_text(strip=True)
+    price_text = price_tag.get_text(strip=True)
+    
+    # Преобразуем цену в число (удаляем символы '$' и пробелы)
+    price = float(re.sub(r"[^\d.,]", "", price_text).replace(",", "."))  # Если разделители — запятые, заменяем их на точку
+    coins.append((code, name, price))
