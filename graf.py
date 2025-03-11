92e7d382047371f4be5324e26f34e578a6519d77
David <davidnikishov1324@gmail.com>
2025-03-11 19:46:39 +0200

    We create the chart in the browser

--- graf.py
+++ graf.py
@@ -1,28 +1,39 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
-# Читаем данные из CSV
+# # Reading data from CSV
 df = pd.read_csv("cryptocurrencies_history.csv")
 
-# Преобразуем столбец с датой в формат datetime
+# We will convert the date column to the `datetime` format.
 df['Date and Time'] = pd.to_datetime(df['Date and Time'])
 
-# Группируем по дате и криптовалюте
+# We group by date and cryptocurrency.
 df_grouped = df[['Date and Time', 'Abbreviation', 'Price ($)']].groupby(['Date and Time', 'Abbreviation']).last().reset_index()
 
-# Строим график
-plt.figure(figsize=(10, 6))
+# We create an interactive chart.
+fig = go.Figure()
 
+# We add a line for each cryptocurrency.
 for coin in df_grouped['Abbreviation'].unique():
     coin_data = df_grouped[df_grouped['Abbreviation'] == coin]
-    plt.plot(coin_data['Date and Time'], coin_data['Price ($)'], label=coin)
+    fig.add_trace(go.Scatter(
+        x=coin_data['Date and Time'],
+        y=coin_data['Price ($)'],
+        mode='lines',
+        name=coin,
+        hoverinfo='x+y+name',  # It displays information on hover.
+        line=dict(width=2)
+    ))
 
-plt.title("Change of cryptocurrency prices")
-plt.xlabel("Date and Time")
-plt.ylabel("Price ($)")
-plt.legend()
-plt.xticks(rotation=45)
-plt.grid(True)
+# Chart settings.
+fig.update_layout(
+    title="Change of cryptocurrency prices",
+    xaxis_title="Date and Time",
+    yaxis_title="Price ($)",
+    legend_title="Crypto",
+    xaxis=dict(tickformat='%Y-%m-%d %H:%M:%S'),
+    hovermode="closest"  # Interactive mode.
+)
 
-plt.tight_layout()
-plt.show()
+# We display the chart.
+fig.show()
