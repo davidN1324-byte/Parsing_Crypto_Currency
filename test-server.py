d9d83a1b7eb1b77af10ef5e597299cf90a6d8be9
David <davidnikishov1324@gmail.com>
2025-03-13 18:19:44 +0200

    The file has been removed from ignoring. A file was added to run local website testing.

---
+++ test-server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    graph_html = ""
+    if os.path.exists("crypto_chart.html"):
+        with open("crypto_chart.html", "r", encoding="utf-8") as file:
+            graph_html = file.read()
+
+    # Reading the analysis text (analysis.txt)
+    analysis_content = ""
+    if os.path.exists("analysis.txt"):
+        with open("analysis.txt", "r", encoding="utf-8") as file:
+            analysis_content = file.read()
+
+    return render_template('index.html', graph_html=graph_html, analysis_content=analysis_content)
+
+if __name__ == "__main__":
+    app.run(debug=True)
